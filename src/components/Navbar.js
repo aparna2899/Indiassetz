0de27300e1e0bd7ae1abac410a4d560c9e68872f
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-12-24 21:43:54 +0530

    create profilepic component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import NavItem from './NavItem';
 
 const navigation = [
@@ -12,29 +14,50 @@ const navigation = [
   { name: 'Red Box', href: '#', icon: 'red-box.png', current: false }
 ];
 
-function Navbar() {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(true);
+
   return (
-    <div className="md:fixed md:inset-y-0 md:flex md:w-56 md:flex-col h-5/6">
-      <div className="flex flex-grow flex-col overflow-y-auto border-gray-200 bg-white pt-5 shadow-2xl rounded-r-[40px]">
-        <div className="flex flex-shrink-0 items-center justify-center px-4">
-          <img className="w-2/3" src="logo.png" alt="Indiassetz" />
+    <div className="md:fixed md:inset-y-0 md:flex md:w-56 md:flex-col lg:h-5/6 block ">
+      <div className="flex flex-grow flex-col overflow-y-auto border-gray-200 bg-white pt-5 shadow-2xl lg:rounded-r-[40px]">
+        <div className="flex flex-shrink-0 items-center justify-between px-4 py-3">
+          <img className="max-[640px]:w-1/4 sm:w-1/4 lg:w-2/3 " src="logo.png" alt="Indiassetz" />
+          <button
+            className="text-gray-500 focus:text-gray-600 hover:text-gray-600 focus:outline-none lg:hidden"
+            onClick={() => setIsOpen(!isOpen)}>
+            <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none">
+              <path
+                d="M4 5h16M4 12h16M4 19h16"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
         </div>
-        <div className="mt-5 mx-6 px-2 pb-4 space-y-1 flex flex-grow flex-col">
-          <nav className="flex-1 border-t border-t-lightgray">
-            {navigation.map((item) => (
-              <NavItem key={item.name} {...item} />
-            ))}
-          </nav>
-          <div className="pb-12 px-2 flex items-center">
-            <div className="w-4">
-              <img src="logout.png" className="w-full" />
+        {isOpen && (
+          <div className="mt-5 mx-6 px-2 pb-4 space-y-1 flex flex-grow flex-col max-[640px]:h-screen max-[640px] sm:h-screen md:h-screen">
+            <nav className="flex-1 border-t border-t-lightgray">
+              {navigation.map((item) => (
+                <NavItem key={item.name} {...item} />
+              ))}
+            </nav>
+            <div className="pb-12 px-2 flex items-center">
+              <div className="w-4">
+                <img src="logout.png" className="w-full" />
+              </div>
+              <a className="ml-4 text-red">Log out</a>
             </div>
-            <a className="ml-4 text-red">Log out</a>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
-}
+};
+
+Navbar.propTypes = {
+  navigation: PropTypes.array
+};
 
 export default Navbar;
