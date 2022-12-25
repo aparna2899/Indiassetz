f75b39d9844af508739bc2066751e96f02bc92c8
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-12-25 19:32:33 +0530

    create client-order-list page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import Container from './components/Container';
 import ClientDetail from './pages/ClientDetail';
 import ClientList from './pages/ClientList';
+import ClientOrderList from './pages/ClientOrderList';
 
 function App() {
   return (
     <div className="App">
       {/* <ClientList /> */}
-      <ClientDetail />
+      {/* <ClientDetail /> */}
+      <ClientOrderList />
     </div>
   );
 }
