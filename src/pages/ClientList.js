c751d3c3036c4191fe569cb765f6bfcd45323172
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-12-27 11:15:48 +0530

    Fix Checkboxes and Add Icon on buttons

--- src/pages/ClientList.js
+++ src/pages/ClientList.js
@@ -4,15 +4,12 @@ import HeadingBox from '../components/HeadingBox';
 import Table from '../components/Table/Table';
 import Button from '../components/Button';
 import Filter from '../components/FilterOptions';
-import Checkbox from '../components/Checkbox';
 import data from '../data/data.json';
 import statusData from '../data/status.json';
 
-// const options = ['All', 'Bangalore', 'Etawah', 'Mainpuri'];
 const statusList = ['All', 'Sell', 'Buy', 'Rent', 'Lease'];
 
 export default function ClientList() {
-  // const [selectedOption, setSelectedOption] = useState('All');
   const [filteredData, setFilteredData] = useState(data);
   const [selectedStatus, setSelectedStatus] = useState('All');
 
@@ -25,10 +22,6 @@ export default function ClientList() {
     }
   };
 
-  const handleChange = (event) => {
-    console.log(event.target.checked);
-  };
-
   return (
     <Container pathToImg="profilepic.png" buttonText="Add Client">
       <HeadingBox heading="Clients" description={`Total ${data.length} "All" Records`} />
@@ -38,9 +31,13 @@ export default function ClientList() {
         handleFilterChange={handleFilterChange}
       />
       <div className="text-right mb-4">
-        <Button buttonText="Filter" className="bg-white text-blue px-8 py-1 hover:bg-lightgray" />
+        <Button buttonText="Filter" className="bg-white text-blue px-8 py-1 hover:bg-lightgray">
+          <div className="w-4 mx-1">
+            <img src="FilterIcon.png" className="w-full" />
+          </div>
+        </Button>
       </div>
-      <Table data={filteredData} handleChange={handleChange} />
+      <Table data={filteredData} />
     </Container>
   );
 }
